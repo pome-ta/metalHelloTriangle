bc4770eb9a0c5a6b812372193393c511e658e525
pome-ta <pometarou93@gmail.com>
2021-11-21 17:48:43 +0900

    up

--- Sources/metalHelloTriangle/AppDelegate.swift
+++ Sources/metalHelloTriangle/AppDelegate.swift
@@ -1,6 +1,13 @@
+// AppDelegate.swift
+
 import Cocoa
 
 let WIDTH = 800
 let HEIGHT = 600
 
-
+Class AppDelegate: NSObject, NSApplicationDelegate {
+    private var mWindow: NSWindow?
+    func applicationDidFinishLaunching(_ aNotifacation: Notification) {
+        // This will be called once when we run the engine
+    }
+}
--- Sources/metalHelloTriangle/main.swift
+++ Sources/metalHelloTriangle/main.swift
@@ -1,1 +1,8 @@
-print("Hello, world!")
+//print("Hello, world!")
+// main.swift
+import Cocoa
+
+let delegate = AppDelegate()
+NSApplication.shared.delegate = delegate
+_ = NSApplicationMain(Command.argc, CommandLine.unsafeArgv)
+
